5d37f0f32f267c3a1454575e75d6088720ea451d
GraphXDoses <xridoses@gmail.com>
2025-05-03 00:53:28 +0300

    Added some fragments & navcontroller with graph [WIP]

--- app/src/main/java/com/parkingapp/hulapark/Activities/HomeScreen.java
+++ app/src/main/java/com/parkingapp/hulapark/Activities/HomeScreen.java
@@ -1,47 +1,46 @@
 package com.parkingapp.hulapark.Activities;
 
-import static android.content.ContentValues.TAG;
-
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.Navigation;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.MenuItem;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.parkingapp.hulapark.R;
+import com.parkingapp.hulapark.Views.CurvedBottomNavigationView;
 import com.parkingapp.hulapark.databinding.ActivityHomeBinding;
-import com.simform.custombottomnavigation.Model;
 
-public class HomeScreen extends AppCompatActivity
+public class HomeScreen extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener
 {
     private ActivityHomeBinding binding;
-
-    private static final int ID_PARKING = 0;
-    private static final int ID_MAP = 1;
-    private static final int ID_WALLET = 2;
-    private static final int ID_HISTORY = 3;
-    private static final String KEY_ACTIVE_INDEX = "activeIndex";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivityHomeBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
-
-        int activeIndex = savedInstanceState == null ? ID_PARKING : savedInstanceState.getInt(KEY_ACTIVE_INDEX);
-
-        binding.SSbottomnav.add(new Model(R.drawable.ic_parking_car, ID_PARKING, ID_PARKING, R.string.navstr_parking, com.simform.custombottomnavigation.R.string.empty_value));
-        binding.SSbottomnav.add(new Model(R.drawable.ic_map, ID_MAP, ID_MAP, R.string.navstr_map, com.simform.custombottomnavigation.R.string.empty_value));
-        binding.SSbottomnav.add(new Model(R.drawable.ic_wallet, ID_WALLET, ID_WALLET, R.string.navstr_wallet, com.simform.custombottomnavigation.R.string.empty_value));
-        binding.SSbottomnav.add(new Model(R.drawable.ic_history, ID_HISTORY, ID_HISTORY, R.string.navstr_history, com.simform.custombottomnavigation.R.string.empty_value));
-
-        binding.SSbottomnav.setSelectedIndex(activeIndex);
+        CurvedBottomNavigationView mView = binding.BottomNavBar;
+        mView.setSelectedItemId(R.id.nav_wallet);
+        mView.setOnNavigationItemSelectedListener(HomeScreen.this);
     }
 
     @Override
-    protected void onSaveInstanceState(@NonNull Bundle outState)
-    {
-        outState.putInt(KEY_ACTIVE_INDEX, binding.SSbottomnav.getSelectedIndex());
-        super.onSaveInstanceState(outState);
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.nav_parking_car:
+                Navigation.findNavController(findViewById(R.id.activeFrag)).navigate(R.id.parkingFrag);
+                break;
+            case R.id.nav_map:
+                Navigation.findNavController(findViewById(R.id.activeFrag)).navigate(R.id.mapFrag);
+                break;
+            case R.id.nav_wallet:
+                break;
+            case R.id.nav_history:
+                break;
+        }
+        return true;
     }
 }
