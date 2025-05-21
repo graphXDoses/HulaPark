8e9ef5820c92111e99b4f4bd98efbfda8756dee5
HawkAfk5 <tilemache@gmail.com>
2025-05-22 01:18:29 +0300

    Added textChangeListener to retain Spannable style

--- app/src/main/java/com/parkingapp/hulapark/Views/SpannableTextView.java
+++ app/src/main/java/com/parkingapp/hulapark/Views/SpannableTextView.java
@@ -1,8 +1,10 @@
 package com.parkingapp.hulapark.Views;
 
 import android.content.Context;
+import android.text.Editable;
 import android.text.SpannableString;
 import android.text.Spanned;
+import android.text.TextWatcher;
 import android.text.style.RelativeSizeSpan;
 import android.util.AttributeSet;
 import android.widget.TextView;
@@ -42,5 +44,36 @@ public class SpannableTextView extends androidx.appcompat.widget.AppCompatTextVi
 
         spannableString.setSpan(new RelativeSizeSpan(2.0f), 0, position + 1, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
         setText(spannableString, TextView.BufferType.SPANNABLE);
+
+        addTextChangedListener(new TextWatcher()
+        {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2)
+            {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2)
+            {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable)
+            {
+                String textAsString = editable.toString();
+                final SpannableString spannableString = new SpannableString(textAsString);
+                int position = textAsString.indexOf('.');
+
+                if(position == -1) return;
+                position -= 1;
+
+                spannableString.setSpan(new RelativeSizeSpan(2.0f), 0, position + 1, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+                removeTextChangedListener(this);
+                setText(spannableString, BufferType.SPANNABLE);
+                addTextChangedListener(this);
+            }
+        });
     }
 }
